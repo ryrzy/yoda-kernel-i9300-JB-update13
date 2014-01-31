fd1affe2bf2a198e6108830a76bf3a8c2e63380b
Ryrzy <ryrzy85@gmail.com>
2014-01-31 11:04:09 +0100

    Revert "PATCH FIX: 3.0.101 patch compile fix - i hope so :)"
    This reverts commit 894c1fbd86fbea3b10380b75c2bca7da11d111a1.

--- drivers/usb/core/hub.c
+++ drivers/usb/core/hub.c
@@ -1943,9 +1943,6 @@ int usb_new_device(struct usb_device *udev)
 
 	/* Tell the world! */
 	announce_device(udev);
-#ifdef CONFIG_SAMSUNG_SMARTDOCK
-	call_battery_notify(udev, 1);
-#endif
 
 	if (udev->serial)
 		add_device_randomness(udev->serial, strlen(udev->serial));
@@ -1955,6 +1952,9 @@ int usb_new_device(struct usb_device *udev)
 		add_device_randomness(udev->manufacturer,
 				      strlen(udev->manufacturer));
 
+#ifdef CONFIG_SAMSUNG_SMARTDOCK
+	call_battery_notify(udev, 1);
+#endif
 	device_enable_async_suspend(&udev->dev);
 	/* Register the device.  The device driver is responsible
 	 * for configuring the device and invoking the add-device
